c13ef5d8ecd775fcb893de6312656496968207de
xiomarapaz <xiomarapaz69@gmail.com>
2020-06-01 05:33:39 +0200

    Exit.java: this file is for the log out of the users

---
+++ src/main/java/Salir.java
@@ -0,0 +1,38 @@
+package main.java;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Logout
+ * @author xioma
+ */
+@WebServlet("/Salir")
+public class Salir extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @throws Servlet
+	 * 
+	 * we only use this method to kill the session of the user
+	 * 
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		HttpSession session = request.getSession(false);
+		session.removeAttribute("user");
+		session.getMaxInactiveInterval();
+		response.sendRedirect("html/index.jsp");
+	}
+
+}
